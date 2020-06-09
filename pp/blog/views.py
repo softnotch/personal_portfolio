05103ab8d6ef452bbaed38da2b740b1512e3f805
kelvin <uchekelvin3@gmail.com>
2020-06-09 08:56:44 +0100

    feat:specified view feat added

--- pp/blog/views.py
+++ pp/blog/views.py
@@ -3,7 +3,7 @@ from .models import AllBlog
 
 # Create your views here.
 def all_blogs(request):
-    blogs = AllBlog.objects.all()
+    blogs = AllBlog.objects.order_by('-date') [:3]
     return render(request,'blog/all_blogs.html' ,{'blogs':blogs})
 
 def about (request):
